739735c0ed63bfb4cb40a42e587cb803a146815b
sjaustirni <22116610+sjaustirni@users.noreply.github.com>
2025-05-04 09:10:56 +0200

    merge

--- main.py
+++ main.py
@@ -25,24 +25,6 @@ def peak_psd(epoch, fs):
     peak_freq = f_range[np.argmax(Pxx_range)]
     return peak_freq
 
-def trca(X):  # X shape: (n_trials, n_channels, n_samples)
-    n_trials, n_channels, n_samples = X.shape
-    S = np.zeros((n_channels, n_channels))
-
-    for i in range(n_trials - 1):
-        x1 = X[i] - X[i].mean(axis=1, keepdims=True)
-        for j in range(i + 1, n_trials):
-            x2 = X[j] - X[j].mean(axis=1, keepdims=True)
-            S += x1 @ x2.T + x2 @ x1.T
-
-    UX = X.transpose(1, 0, 2).reshape(n_channels, -1)
-    UX = UX - UX.mean(axis=1, keepdims=True)
-    Q = UX @ UX.T
-
-    eigvals, eigvecs = eigh(S, Q)
-    W = eigvecs[:, np.argsort(eigvals)[::-1]]
-    return W
-
 class PSDAClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self, fs, target_freqs):
         self.fs = fs
@@ -68,32 +50,54 @@ class PSDAClassifier(BaseEstimator, ClassifierMixin):
         return np.array(preds)
 
 class TRCAClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, fs, target_freqs):
+    def __init__(self, fs, num_fbs=1):
+        self.filters_ = None
+        self.templates_ = None
+        self.classes_ = None
         self.fs = fs
-        self.target_freqs = target_freqs
-        self.filters_ = {}
+        self.num_fbs = num_fbs
+
+    def trca(self, X):
+        n_trials, n_channels, n_samples = X.shape
+        S = np.zeros((n_channels, n_channels))
+        for i in range(n_trials - 1):
+            x1 = X[i] - X[i].mean(axis=1, keepdims=True)
+            for j in range(i + 1, n_trials):
+                x2 = X[j] - X[j].mean(axis=1, keepdims=True)
+                S += x1 @ x2.T + x2 @ x1.T
+        UX = X.transpose(1, 0, 2).reshape(n_channels, -1)
+        UX -= UX.mean(axis=1, keepdims=True)
+        Q = UX @ UX.T
+        eigvals, eigvecs = eigh(S, Q)
+        W = eigvecs[:, np.argsort(eigvals)[::-1]]
+        return W
 
     def fit(self, X, y):
+        # X shape: (n_trials, n_channels, n_samples)
+        # y shape: (n_trials,)
         self.classes_ = np.unique(y)
+        self.templates_ = {}
+        self.filters_ = {}
         for label in self.classes_:
             trials = X[y == label]
-            W = trca(trials)
-            self.filters_[label] = W[:, 0]  # Keep first component
+            W = self.trca(trials)
+            self.filters_[label] = W[:, 0]  # use first component
+            self.templates_[label] = np.mean([W[:, 0].T @ trial for trial in trials], axis=0)
         return self
 
     def predict(self, X):
+        # X shape: (n_trials, n_channels, n_samples)
         preds = []
         for epoch in X:
-            error = {}
-            for label, w in self.filters_.items():
-                filtered = w.T @ epoch
-                peak_freq = peak_psd(filtered, self.fs)
-                harmonics = [label, label * 2, label / 2]
-                distances = [abs(peak_freq - h) for h in harmonics]
-                error[label] = min(distances)
-            preds.append(min(error, key=error.get))
+            corr_scores = []
+            for label in self.classes_:
+                w = self.filters_[label]
+                projected = w.T @ epoch
+                template = self.templates_[label]
+                r = np.corrcoef(projected, template)[0, 1]
+                corr_scores.append(r)
+            preds.append(self.classes_[np.argmax(corr_scores)])
         return np.array(preds)
-
 # Dataset definition
 dataset = [
     {'subject': 1, 'session': 1, 'path': 'data/subject_1_fvep_led_training_1.EDF'},
@@ -135,8 +139,7 @@ for session in dataset:
     edf_file_path = session['path']
 
     print(f"\nProcessing Subject {subject}, Session {session_num}")
-    raw = load_data(edf_file_path)
-    print(raw.info)
+    raw = load_data(edf_file_path, verbose=False)
     fs = int(raw.info['sfreq'])
 
     events = find_events(raw, stim_channel='stim', verbose=False)
@@ -151,24 +154,31 @@ for session in dataset:
 
     X = epochs.get_data()  # (n_epochs, n_channels, n_samples)
     y = np.array([FREQUENCIES[event[2]] for event in epochs.events])
+    n_samples = X.shape[0]
 
-    # PSDA
+    # Split for TRCA: train on first half, test on second
+    half = n_samples // 2
+    X_train, y_train = X[:half], y[:half]
+    X_test, y_test = X[half:], y[half:]
+
+    # PSDA: train and test on full
     psda = PSDAClassifier(fs=fs, target_freqs=FREQUENCIES)
     psda.fit(X, y)
     y_pred_psda = psda.predict(X)
 
-    # TRCA
-    trca_clf = TRCAClassifier(fs=fs, target_freqs=FREQUENCIES)
-    trca_clf.fit(X, y)
-    y_pred_trca = trca_clf.predict(X)
+    # TRCA: train on first half, test on second
+    trca_clf = TRCAClassifier(fs=fs)
+    trca_clf.fit(X_train, y_train)
+    y_pred_trca = trca_clf.predict(X_test)
 
     # Evaluation
-    print("True Labels:   ", y.tolist())
-    print("PSDA Predict:  ", y_pred_psda.tolist())
-    print("TRCA Predict:  ", y_pred_trca.tolist())
+    print("True Labels (all):   ", y.tolist())
+    print("PSDA Predict (all):  ", y_pred_psda.tolist())
+    print("PSDA Accuracy (all): ", metrics.accuracy_score(y, y_pred_psda))
 
-    print(f"PSDA Accuracy: {metrics.accuracy_score(y, y_pred_psda):.3f}")
-    print(f"TRCA Accuracy: {metrics.accuracy_score(y, y_pred_trca):.3f}")
+    print("True Labels (TRCA test):   ", y_test.tolist())
+    print("TRCA Predict (half test):  ", y_pred_trca.tolist())
+    print("TRCA Accuracy (half test): ", metrics.accuracy_score(y_test, y_pred_trca))
 
     # Plot confusion matrices for both classifiers
     psda_cm_plot = plot_confusion_matrix(y, y_pred_psda, f'PSDA - subject:{subject}, session:{session_num}', FREQUENCIES)
